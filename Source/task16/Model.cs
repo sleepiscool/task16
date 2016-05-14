b2e87a9f8fe21212c76a4bba8b309a8b6dff2be4
den1sqaa <denis007_1996@mail.ru>
2016-05-14 11:49:07 +0300

    изменил подключение к бд

---
+++ Source/task16/Model.cs
@@ -0,0 +1,41 @@
+﻿using MySql.Data;
+using System.Data.Common;
+using System.Data.Entity;
+
+namespace EF6
+{
+    // Code-Based Configuration and Dependency resolution
+    [DbConfigurationType(typeof(MySqlEFConfiguration))]
+    public class Parking : DbContext
+    {
+        public DbSet<Car> Cars { get; set; }
+
+        public Parking() : base()
+        {
+
+        }
+
+        // Constructor to use on a DbConnection that is already opened
+        public Parking(DbConnection existingConnection, bool contextOwnsConnection): base(existingConnection, contextOwnsConnection)
+        {
+
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Car>().MapToStoredProcedures();
+        }
+    }
+
+    public class Car
+    {
+        public int CarId { get; set; }
+
+        public string Model { get; set; }
+
+        public int Year { get; set; }
+
+        public string Manufacturer { get; set; }
+    }
+}
--- Source/task16/Program.cs
+++ Source/task16/Program.cs
@@ -14,6 +14,7 @@ namespace task16
         [STAThread]
         static void Main()
         {
+            MySqll.ConnectToDataBAse();
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
