44455cc6ae2d1f21e3680427d0836b51ff8d162c
buyskikh20 <forward2010-96@mail.ru>
2016-05-17 22:50:17 +0400

    form

---
+++ task16/Source/Aapplication_model/application_model.Designer.cs
@@ -0,0 +1,274 @@
+﻿namespace Aapplication_model
+{
+    partial class application_model
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.ID = new System.Windows.Forms.TextBox();
+            this.Surname = new System.Windows.Forms.TextBox();
+            this.Name = new System.Windows.Forms.TextBox();
+            this.Patronymic = new System.Windows.Forms.TextBox();
+            this.Organization = new System.Windows.Forms.TextBox();
+            this.Position = new System.Windows.Forms.TextBox();
+            this.Email = new System.Windows.Forms.TextBox();
+            this.NumberPhone = new System.Windows.Forms.TextBox();
+            this.Add = new System.Windows.Forms.Button();
+            this.Update = new System.Windows.Forms.Button();
+            this.Delete = new System.Windows.Forms.Button();
+            this.Search = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "SurName";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 79);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 106);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Patronymic";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 134);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(66, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Organization";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 163);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(44, 13);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Position";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 193);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(32, 13);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "Email";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(12, 222);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(75, 13);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "NumberPhone";
+            // 
+            // ID
+            // 
+            this.ID.Location = new System.Drawing.Point(110, 17);
+            this.ID.Name = "ID";
+            this.ID.Size = new System.Drawing.Size(173, 20);
+            this.ID.TabIndex = 10;
+            // 
+            // Surname
+            // 
+            this.Surname.Location = new System.Drawing.Point(110, 45);
+            this.Surname.Name = "Surname";
+            this.Surname.Size = new System.Drawing.Size(173, 20);
+            this.Surname.TabIndex = 11;
+            // 
+            // Name
+            // 
+            this.Name.Location = new System.Drawing.Point(110, 72);
+            this.Name.Name = "Name";
+            this.Name.Size = new System.Drawing.Size(173, 20);
+            this.Name.TabIndex = 12;
+            // 
+            // Patronymic
+            // 
+            this.Patronymic.Location = new System.Drawing.Point(110, 99);
+            this.Patronymic.Name = "Patronymic";
+            this.Patronymic.Size = new System.Drawing.Size(173, 20);
+            this.Patronymic.TabIndex = 13;
+            // 
+            // Organization
+            // 
+            this.Organization.Location = new System.Drawing.Point(110, 127);
+            this.Organization.Name = "Organization";
+            this.Organization.Size = new System.Drawing.Size(173, 20);
+            this.Organization.TabIndex = 14;
+            // 
+            // Position
+            // 
+            this.Position.Location = new System.Drawing.Point(110, 156);
+            this.Position.Name = "Position";
+            this.Position.Size = new System.Drawing.Size(173, 20);
+            this.Position.TabIndex = 15;
+            // 
+            // Email
+            // 
+            this.Email.Location = new System.Drawing.Point(110, 186);
+            this.Email.Name = "Email";
+            this.Email.Size = new System.Drawing.Size(173, 20);
+            this.Email.TabIndex = 16;
+            // 
+            // NumberPhone
+            // 
+            this.NumberPhone.Location = new System.Drawing.Point(110, 215);
+            this.NumberPhone.Name = "NumberPhone";
+            this.NumberPhone.Size = new System.Drawing.Size(173, 20);
+            this.NumberPhone.TabIndex = 17;
+            // 
+            // Add
+            // 
+            this.Add.Location = new System.Drawing.Point(15, 259);
+            this.Add.Name = "Add";
+            this.Add.Size = new System.Drawing.Size(75, 23);
+            this.Add.TabIndex = 18;
+            this.Add.Text = "Добавить";
+            this.Add.UseVisualStyleBackColor = true;
+            // 
+            // Update
+            // 
+            this.Update.Location = new System.Drawing.Point(110, 259);
+            this.Update.Name = "Update";
+            this.Update.Size = new System.Drawing.Size(75, 23);
+            this.Update.TabIndex = 19;
+            this.Update.Text = "Обновить";
+            this.Update.UseVisualStyleBackColor = true;
+            // 
+            // Delete
+            // 
+            this.Delete.Location = new System.Drawing.Point(208, 259);
+            this.Delete.Name = "Delete";
+            this.Delete.Size = new System.Drawing.Size(75, 23);
+            this.Delete.TabIndex = 20;
+            this.Delete.Text = "Удалить";
+            this.Delete.UseVisualStyleBackColor = true;
+            // 
+            // Search
+            // 
+            this.Search.Location = new System.Drawing.Point(303, 259);
+            this.Search.Name = "Search";
+            this.Search.Size = new System.Drawing.Size(75, 23);
+            this.Search.TabIndex = 21;
+            this.Search.Text = "Поиск";
+            this.Search.UseVisualStyleBackColor = true;
+            // 
+            // application_model
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(870, 500);
+            this.Controls.Add(this.Search);
+            this.Controls.Add(this.Delete);
+            this.Controls.Add(this.Update);
+            this.Controls.Add(this.Add);
+            this.Controls.Add(this.NumberPhone);
+            this.Controls.Add(this.Email);
+            this.Controls.Add(this.Position);
+            this.Controls.Add(this.Organization);
+            this.Controls.Add(this.Patronymic);
+            this.Controls.Add(this.Name);
+            this.Controls.Add(this.Surname);
+            this.Controls.Add(this.ID);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "application_model";
+            this.Text = "Aapplication_model";
+            this.Load += new System.EventHandler(this.application_model_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox ID;
+        private System.Windows.Forms.TextBox Surname;
+        private System.Windows.Forms.TextBox Name;
+        private System.Windows.Forms.TextBox Patronymic;
+        private System.Windows.Forms.TextBox Organization;
+        private System.Windows.Forms.TextBox Position;
+        private System.Windows.Forms.TextBox Email;
+        private System.Windows.Forms.TextBox NumberPhone;
+        private System.Windows.Forms.Button Add;
+        private System.Windows.Forms.Button Update;
+        private System.Windows.Forms.Button Delete;
+        private System.Windows.Forms.Button Search;
+    }
+}
+
---
+++ task16/Source/Aapplication_model/application_model.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aapplication_model
+{
+    public partial class application_model : Form
+    {
+        public application_model()
+        {
+            InitializeComponent();
+        }
+
+        private void application_model_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
